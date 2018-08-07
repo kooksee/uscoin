a2694a28ac251e2d1c044d06f3294a8f248d5754
白云辉 <baiyunhui@yuanben.org>
2018-08-07 19:19:07 +0800

    init

---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/tendermint/tendermint/libs/cli"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/keys"
+	"github.com/cosmos/cosmos-sdk/client/lcd"
+	"github.com/cosmos/cosmos-sdk/client/rpc"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+
+	"github.com/cosmos/cosmos-sdk/version"
+	authcmd "github.com/cosmos/cosmos-sdk/x/auth/client/cli"
+	bankcmd "github.com/cosmos/cosmos-sdk/x/bank/client/cli"
+	ibccmd "github.com/cosmos/cosmos-sdk/x/ibc/client/cli"
+
+	"github.com/kooksee/uscoin/app"
+	"github.com/kooksee/uscoin/tps"
+	coolcmd "github.com/kooksee/uscoin/x/cool/client/cli"
+	powcmd "github.com/kooksee/uscoin/x/pow/client/cli"
+	simplestakingcmd "github.com/kooksee/uscoin/x/simplestake/client/cli"
+	"github.com/kooksee/uscoin/cmds"
+)
+
+// rootCmd is the entry point for this binary
+var (
+	rootCmd = &cobra.Command{
+		Use:   "democli",
+		Short: "Democoin light-client",
+	}
+)
+
+func main() {
+	// disable sorting
+	cobra.EnableCommandSorting = false
+
+	// get the codec
+	cdc := app.MakeCodec()
+
+	// TODO: setup keybase, viper object, etc. to be passed into
+	// the below functions and eliminate global vars, like we do
+	// with the cdc
+
+	// add standard rpc, and tx commands
+	rpc.AddCommands(rootCmd)
+	rootCmd.AddCommand(client.LineBreak)
+	tx.AddCommands(rootCmd, cdc)
+	rootCmd.AddCommand(client.LineBreak)
+
+	// add query/post commands (custom to binary)
+	// start with commands common to basecoin
+	rootCmd.AddCommand(
+		client.GetCommands(
+			authcmd.GetAccountCmd("acc", cdc, tps.GetAccountDecoder(cdc)),
+		)...)
+	rootCmd.AddCommand(
+		client.PostCommands(
+			bankcmd.SendTxCmd(cdc),
+		)...)
+	rootCmd.AddCommand(
+		client.PostCommands(
+			ibccmd.IBCTransferCmd(cdc),
+		)...)
+	rootCmd.AddCommand(
+		client.PostCommands(
+			ibccmd.IBCRelayCmd(cdc),
+			simplestakingcmd.BondTxCmd(cdc),
+		)...)
+	rootCmd.AddCommand(
+		client.PostCommands(
+			simplestakingcmd.UnbondTxCmd(cdc),
+		)...)
+	// and now democoin specific commands
+	rootCmd.AddCommand(
+		client.PostCommands(
+			coolcmd.QuizTxCmd(cdc),
+			coolcmd.SetTrendTxCmd(cdc),
+			powcmd.MineCmd(cdc),
+		)...)
+
+	// add proxy, version and key info
+	rootCmd.AddCommand(
+		client.LineBreak,
+		lcd.ServeCommand(cdc),
+		keys.Commands(),
+		client.LineBreak,
+		version.VersionCmd,
+		cmds.RunNodeCmd(),
+		cmds.VersionCmd,
+	)
+
+	// prepare and add flags
+	if err := cli.PrepareMainCmd(rootCmd, "BC", os.ExpandEnv("$HOME/.democli")).Execute(); err != nil {
+		// handle with #870
+		panic(err)
+	}
+}
---
+++ version/version.go
@@ -0,0 +1,4 @@
+package version
+const Version = "v0.3.0"
+const BuildVersion = "2018-08-07T19:18:06+0800"
+const GitCommit = "650ad463"
